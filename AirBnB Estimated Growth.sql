a1943db97d093743c661eb70b6b40d49442dbbaa
Lola Olumoyin <61130199+Lolusstarlet@users.noreply.github.com>
2022-09-04 19:06:07 -0600

    Update AirBnB Estimated Growth.sql

--- AirBnB Estimated Growth.sql
+++ AirBnB Estimated Growth.sql
@@ -1,13 +1,13 @@
-DATASOURCE- https://www.kaggle.com/datasets/oindrilasen/la-airbnb-listings/discussion?resource=download
+--DATASOURCE- https://www.kaggle.com/datasets/oindrilasen/la-airbnb-listings/discussion?resource=download
 
-Download the file and migrate into SSMS with SQL Server 2019 Import and Export Data
-Source- Excel
-Destination- SQL Server Native Client 11
-Select a DB and load.
+--Download the file and migrate into SSMS with SQL Server 2019 Import and Export Data
+--Source- Excel
+--Destination- SQL Server Native Client 11
+--Select a DB and load.
 
-QUESTION- Estimate the growth of Airbnb each year using the number of hosts registered as the growth metric. The rate of growth is calculated by taking ((number of hosts registered in the current year - number of hosts registered in the previous year) / the number of hosts registered in the previous year) * 100.
-Output the year, number of hosts in the current year, number of hosts in the previous year, and the rate of growth. Round the rate of growth to the nearest percent and order the result in the ascending order based on the year.
-Assume that the dataset consists only of unique hosts, meaning there are no duplicate hosts listed.
+--QUESTION- Estimate the growth of Airbnb each year using the number of hosts registered as the growth metric. The rate of growth is calculated by taking ((number of hosts registered in the current year - number of hosts registered in the previous year) / the number of hosts registered in the previous year) * 100.
+--Output the year, number of hosts in the current year, number of hosts in the previous year, and the rate of growth. Round the rate of growth to the nearest percent and order the result in the ascending order based on the year.
+--Assume that the dataset consists only of unique hosts, meaning there are no duplicate hosts listed.
 
 
 	
